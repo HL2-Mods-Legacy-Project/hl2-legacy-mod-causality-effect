ca00d8d6396e0cecd07493a29ddcc63bfb135c49
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-19 19:00:44 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
-  -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -Version "1.0.0" `
+  -OutputFilePrefix "Causality_Effect" `
+  -ModName "Causality Effect" `
+  -ModFolder "CausalityEffect" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/causality-effect/CausalityEffect-Original.zip" `
+  -ModBaseFilesUrlHash "bf4f442d585512494bbf5b2e9eeab4624148c3d039f0a82df762c168fbedb80d" `
+  -ModReadmePath "Causality Effect Read Me First.txt" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
